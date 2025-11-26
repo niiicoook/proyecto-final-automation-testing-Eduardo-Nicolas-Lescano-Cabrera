b8dbf1a1fb142a710d9ae49b75a60c5601cc493f
nico lesca <nicolescano004@gmail.com>
2025-11-26 16:25:31 -0300

    agregue los impor de sys y os ya que no funcionaba sin eso

--- test/conftest.py
+++ test/conftest.py
@@ -7,12 +7,8 @@ import os
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(ROOT)
 
-
-
 import pytest
 from utils.funciones import configuracion
-
-
 
 @pytest.fixture
 def driver_conf():
